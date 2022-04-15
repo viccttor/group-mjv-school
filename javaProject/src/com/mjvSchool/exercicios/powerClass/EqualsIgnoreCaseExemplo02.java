45aa7ac3ae0047d4f164810a5523a07e413e2ef0
Victor Henrique <vhsdm7@gmail.com>
2022-04-15 16:32:48 -0300

    Exemplos toString

--- javaProject/src/com/mjvSchool/exercicios/powerClass/EqualsIgnoreCaseExemplo02.java
+++ javaProject/src/com/mjvSchool/exercicios/powerClass/EqualsIgnoreCaseExemplo02.java
@@ -1,5 +1,7 @@
 package com.mjvSchool.exercicios.powerClass;
 
+import java.util.Scanner;
+
 public class EqualsIgnoreCaseExemplo02 {
 
 	public static void main(String[] args) {
@@ -9,7 +11,37 @@ public class EqualsIgnoreCaseExemplo02 {
 		 * 
 		 */
 
-		// Exemplo 02 - Jogo de adivinhação
+		// Exemplo 02 - Dia da Semana
+		Scanner sEntrada = new Scanner(System.in);
+		
+		// Informação
+		System.out.println("Dia da Semana - Digite um número entre 1 e 7 para que seja informado o dia da semana");
+		System.out.println();
+		
+		// Estrutura
+		
+		System.out.print("Digite o dia da semana: ");
+		String nomeDaSemana = sEntrada.next();
+		
+		if ("domingo".equalsIgnoreCase(nomeDaSemana)) {
+			System.out.printf(nomeDaSemana + " é o dia da semana de número: "  + 1 );
+		} else if ("segunda".equalsIgnoreCase(nomeDaSemana)) {
+			System.out.printf(nomeDaSemana + " é o dia da semana de número: "  + 2 );
+		} else if ("terça".equalsIgnoreCase(nomeDaSemana)
+				|| "terca".equalsIgnoreCase(nomeDaSemana)) {
+			System.out.printf(nomeDaSemana + " é o " + 3 + " dia da semana!");
+		} else if ("quarta".equalsIgnoreCase(nomeDaSemana)) {
+			System.out.printf(nomeDaSemana + " é o dia da semana de número: "  + 4);
+		} else if ("quinta".equalsIgnoreCase(nomeDaSemana)) {
+			System.out.printf(nomeDaSemana + " é o dia da semana de número: " + 5 );
+		} else if ("sexta".equalsIgnoreCase(nomeDaSemana)) {
+			System.out.printf(nomeDaSemana + " é o dia da semana de número: "  + 6 );
+		} else if ("sábado".equalsIgnoreCase(nomeDaSemana)
+				|| "sabado".equalsIgnoreCase(nomeDaSemana)) {
+			System.out.printf(nomeDaSemana + " é o dia da semana de número: " + 7 );
+		} else System.out.println("Dia inválido!");
+	
+		sEntrada.close();
 
 	}
 
--- javaProject/src/com/mjvSchool/exercicios/powerClass/ExemploGeral.java
+++ javaProject/src/com/mjvSchool/exercicios/powerClass/ExemploGeral.java
@@ -3,8 +3,9 @@ package com.mjvSchool.exercicios.powerClass;
 public class ExemploGeral {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
