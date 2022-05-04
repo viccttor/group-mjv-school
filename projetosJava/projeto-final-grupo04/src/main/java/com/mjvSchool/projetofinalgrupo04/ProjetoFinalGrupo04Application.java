1557ae6cc8e2eaade3e461dfb4f2027884189169
Victor Henrique <vhsdm7@gmail.com>
2022-05-03 23:15:27 -0300

    Update

---
+++ projetosJava/projeto-final-grupo04/src/main/java/com/mjvSchool/projetofinalgrupo04/ProjetoFinalGrupo04Application.java
@@ -0,0 +1,13 @@
+package com.mjvSchool.projetofinalgrupo04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalGrupo04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalGrupo04Application.class, args);
+	}
+
+}
---
+++ projetosJava/projeto-final-grupo04/src/test/java/com/mjvSchool/projetofinalgrupo04/ProjetoFinalGrupo04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjvSchool.projetofinalgrupo04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalGrupo04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
