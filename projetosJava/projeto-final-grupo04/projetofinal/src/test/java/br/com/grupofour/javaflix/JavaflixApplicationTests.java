3683db5df8c24af422ad92275981857f5c0b9bfb
DiegoCardosoDev <diegocardososcramento@gmail.com>
2022-05-04 18:33:23 -0300

    refatory

---
+++ projetosJava/projeto-final-grupo04/projetofinal/src/test/java/br/com/grupofour/javaflix/JavaflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupofour.javaflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
