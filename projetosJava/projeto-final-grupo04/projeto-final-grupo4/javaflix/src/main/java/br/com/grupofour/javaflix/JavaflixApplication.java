f0a358bb9455bce6545dc2ac1f8466071b9a4c3d
DiegoCardosoDev <diegocardososcramento@gmail.com>
2022-05-04 18:08:31 -0300

    move my projeto para minha branch

---
+++ projetosJava/projeto-final-grupo04/projeto-final-grupo4/javaflix/src/main/java/br/com/grupofour/javaflix/JavaflixApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupofour.javaflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaflixApplication.class, args);
+	}
+
+}
---
+++ projetosJava/projeto-final-grupo04/projeto-final-grupo4/javaflix/src/main/java/br/com/grupofour/javaflix/controllers/FilmesController.java
@@ -0,0 +1,8 @@
+package br.com.grupofour.javaflix.controllers;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class FilmesController {
+}
