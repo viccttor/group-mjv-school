d4cfcb84662ec64008244aed00dd43e35cd12d6a
DiegoCardosoDev <diegocardososcramento@gmail.com>
2022-05-04 12:52:53 -0300

    diegocardoso, add repo

---
+++ projetosJava/projeto-final-grupo04/javaflix/src/main/java/br/com/groupfour/javaflix/javaflix/repositories/FilmesRepository.java
@@ -0,0 +1,7 @@
+package br.com.groupfour.javaflix.javaflix.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmesRepository extends JpaRepository {
+    
+}
