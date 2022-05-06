78c9b3432e32e9ee05b34509f1a319f80ee0ac1b
dcardososo <diego.sacramento@engdb.com.br>
2022-05-06 16:19:52 -0300

    ecormerce

---
+++ projetosJava/ecormerce/src/main/java/br/com/grupofour/ecormerce/EcormerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupofour.ecormerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcormerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcormerceApplication.class, args);
+	}
+
+}
---
+++ projetosJava/ecormerce/src/test/java/br/com/grupofour/ecormerce/EcormerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupofour.ecormerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcormerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
