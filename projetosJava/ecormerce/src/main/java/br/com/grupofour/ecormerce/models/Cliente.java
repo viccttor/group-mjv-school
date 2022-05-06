28fd4223a9d1ce0c141a5872044fbeb1d5c9ecb6
dcardososo <diego.sacramento@engdb.com.br>
2022-05-06 16:45:26 -0300

    classe produto implementada

--- projetosJava/ecormerce/src/main/java/br/com/grupofour/ecormerce/models/Cliente.java
+++ projetosJava/ecormerce/src/main/java/br/com/grupofour/ecormerce/models/Cliente.java
@@ -1,19 +1,14 @@
 package br.com.grupofour.ecormerce.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
 @Entity(name = "tb_cliente")
 public class Cliente {
 
@@ -22,4 +17,39 @@ public class Cliente {
     private Long id;
     private String nome;
     private String email;
+
+    public Cliente() {
+
+    }
+
+    public Cliente(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
---
+++ projetosJava/ecormerce/src/main/java/br/com/grupofour/ecormerce/models/Produto.java
@@ -0,0 +1,55 @@
+package br.com.grupofour.ecormerce.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Entity(name = "tb_produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome_produto;
+    private Double preco;
+
+    public Produto() {
+    }
+
+    public Produto(Long id, String nome_produto, Double preco) {
+        this.id = id;
+        this.nome_produto = nome_produto;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome_produto() {
+        return nome_produto;
+    }
+
+    public void setNome_produto(String nome_produto) {
+        this.nome_produto = nome_produto;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
