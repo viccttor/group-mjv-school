291b38124b698ea3c110366749f73f4fb5a980e9
dcardososo <diego.sacramento@engdb.com.br>
2022-05-06 16:39:02 -0300

    classe cliente implementada

---
+++ projetosJava/ecormerce/src/main/java/br/com/grupofour/ecormerce/models/Cliente.java
@@ -0,0 +1,25 @@
+package br.com.grupofour.ecormerce.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+}
